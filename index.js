4c860c2b667290562910a6550f3b383bb796e54b
Matvei Antipov <matvei.antipov@nordigy.ru>
2021-10-06 23:23:45 +0300

    adds https

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
-const port = 8080;
+const port = 443;
+
+const key = fs.readFileSync('privkey.pem');
+const cert = fs.readFileSync('fullchain.pem');
+const options = {
+  key,
+  cert,
+};
 
 app.get('/', (req, res) => {
   res.header('X-Author', 'itmo225071').header('Access-Control-Allow-Origin', '*').send('itmo225071');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
+  console.log(`server starting on port : ${port}`);
 });
