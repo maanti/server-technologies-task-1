4a7bbe2718e72bbec077a0439875abf271c73869
Matvei Antipov <matvei.antipov@nordigy.ru>
2021-10-08 19:53:05 +0300

    task2

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 const cors = require('cors');
 
 const app = express();
-const port = 443;
-
-const key = fs.readFileSync('privkey.pem');
-const cert = fs.readFileSync('fullchain.pem');
-const options = {
-  key,
-  cert,
-};
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.header('X-Author', 'itmo225071').send('itmo225071');
+app.get('/login/', (req, res) => {
+  res.header('Content-Type', 'text/plain; charset=UTF-8 ').send('itmo225071');
 });
 
-const server = https.createServer(options, app);
+app.get('/promise/', (req, res) => {
+  res.header('Content-Type', 'text/plain; charset=UTF-8 ').send(`
+    function task(x) {
+      return new Promise((resolve, reject) => {
+        if (x < 18) {
+          return resolve('yes');
+        }
+        return reject('no');
+      });
+    }
+`);
+});
 
-server.listen(port, () => {
+app.get('/fetch/', (req, res) => {
+  res.header('Content-type', 'text/html; charset=UTF-8').sendFile(`${__dirname}/index.html`);
+});
+
+app.listen(port, () => {
   console.log(`server starting on port : ${port}`);
 });
