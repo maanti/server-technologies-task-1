10cbf3fbb02f86f7ddce07603c0a5e8e2405b84f
Matvei Antipov <matvei.antipov@nordigy.ru>
2021-10-11 23:41:51 +0300

    task 4

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors());
+const corsOptions = {
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['x-test', 'Content-Type', 'Accept', 'Access-Control-Allow-Headers'],
+  origin: '*',
+};
 
-app.get('/login/', (req, res) => {
-  res.header('Content-Type', 'text/plain; charset=UTF-8 ').send('itmo225071');
+app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  let data = '';
+  req.setEncoding('utf8');
+  req.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  req.on('end', () => {
+    req.body = data;
+    next();
+  });
 });
 
-app.get('/promise/', (req, res) => {
-  res.header('Content-Type', 'text/plain; charset=UTF-8 ').send(`
-    function task(x) {
-      return new Promise((resolve, reject) => {
-        if (x < 18) {
-          return resolve('yes');
-        }
-        return reject('no');
-      });
-    }
-`);
-});
-
-app.get('/fetch/', (req, res) => {
-  res.header('Content-type', 'text/html; charset=UTF-8').sendFile(`${__dirname}/index.html`);
+app.all('/result4/', (req, res) => {
+  res.json({
+    message: 'itmo225071',
+    'x-result': req.header('x-test'),
+    'x-body': req.body,
+  });
 });
 
 app.listen(port, () => {
